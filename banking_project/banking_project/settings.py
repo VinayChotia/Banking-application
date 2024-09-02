b905d84ebdbf6d4dfe8066d2dcfe9132b49294b8
VinayChotia <vinaychotia121@gmail.com>
2024-09-02 10:01:27 +0530

    added_cors_headers

--- banking_project/banking_project/settings.py
+++ banking_project/banking_project/settings.py
@@ -92,7 +92,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # "corsheaders.middleware.CorsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'corsheaders.middleware.CorsMiddleware',
